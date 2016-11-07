97566cb0f03ef2f7520913537f50f7d4dfdf877e
Yeray Ruiz <yeray.1997.yr@gmail.com>
2016-11-07 13:49:34 +0100

    Some code added

---
+++ app/src/main/java/com/phile/yrj/takethebullfighterwiththehorns/New_Activity.java
@@ -0,0 +1,13 @@
+package com.phile.yrj.takethebullfighterwiththehorns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class New_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
